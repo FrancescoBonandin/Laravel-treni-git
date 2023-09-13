c424f7df0f09f33a0ff5247f444ad4c67533edde
FrancescoBoco <francesco.bocoux@gmail.com>
2023-09-13 17:01:08 +0200

    ADD main

--- resources/views/partials/main-content.blade.php
+++ resources/views/partials/main-content.blade.php
@@ -2,8 +2,23 @@
 
 
 
+<div class="container">
+    <div class="row">
+        @for ($i = 0; $i < 5; $i++)
+            <div class="col-sm-2 col-md-6 col-lg-6">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Treni</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        @endfor
+    </div>
+</div>
 
-    HOMEPAGE
 
 
 
@@ -11,5 +26,4 @@
 
 
 
-    
 </main>
